1319c667a3248146bf94cdfb4a30dd984ccc7610
Mohamed Yassin <meddyassin@hotmail.com>
2021-09-19 05:04:10 +0200

    push

--- src/Timer.js
+++ src/Timer.js
@@ -7,12 +7,20 @@ import {Context} from "./Context"
 function Timer() {
     const {background, handleBackground, trackingNumber, setTrackingNumber} = useContext(Context)
     handleBackground(trackingNumber)
+    const design = {
+        border:"2px solid indianred",
+        borderRadius: "10px",
+        background:"indianred",
+        cursor: "pointer",
+        color:"white",
+        padding:"10px"
+    }
     return (
         <>
          <div className="timer">
-          <div onClick={() => setTrackingNumber(1)}>work time</div>
-          <div onClick={() => setTrackingNumber(2)}>short breack</div>
-          <div onClick={() => setTrackingNumber(3)}>long breack</div>
+          <div onClick={() => setTrackingNumber(1)} style={trackingNumber === 1 ? design : {}}>work time</div>
+          <div onClick={() => setTrackingNumber(2)} style={trackingNumber === 2 ? design : {}}>short breack</div>
+          <div onClick={() => setTrackingNumber(3)} style={trackingNumber === 3 ? design : {}}>long breack</div>
          </div>
          <div>
           {trackingNumber === 1 && <PomodoroTimer />}
